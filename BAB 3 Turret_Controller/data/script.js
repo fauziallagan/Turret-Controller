20c39f4fe8a77c60c44c46dd72bdf848aed08d9e
muropraktikum <muro.gundar@gmail.com>
2022-09-08 00:12:20 +0700

    initial Commit

---
+++ BAB 3 Turret_Controller/data/script.js
@@ -0,0 +1,70 @@
+var gateway = `ws://${window.location.hostname}/ws`;
+var websocket;
+window.addEventListener('load', onload);
+
+function onload(event) {
+    initWebSocket();
+}
+
+function getValues(){
+    websocket.send("getValues");
+}
+
+function initWebSocket() {
+    console.log('Trying to open a WebSocket connection…');
+    websocket = new WebSocket(gateway);
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
+    websocket.onmessage = onMessage;
+}
+
+function onOpen(event) {
+    console.log('Connection opened');
+    getValues();
+}
+
+function onClose(event) {
+    console.log('Connection closed');
+    setTimeout(initWebSocket, 2000);
+}
+
+function updateSliderPWM(element) {
+    var sliderNumber = element.id.charAt(element.id.length-1);
+    var sliderValue = document.getElementById(element.id).value;
+    document.getElementById("sliderValue"+sliderNumber).innerHTML = sliderValue;
+    console.log(sliderValue);
+    websocket.send(sliderNumber+"s"+sliderValue.toString());
+}
+
+function onMessage(event) {
+    console.log(event.data);
+    var myObj = JSON.parse(event.data);
+    var keys = Object.keys(myObj);
+
+    for (var i = 0; i < keys.length; i++){
+        var key = keys[i];
+        document.getElementById(key).innerHTML = myObj[key];
+        document.getElementById("slider"+ (i+1).toString()).value = myObj[key];
+    }
+}
+var video = document.querySelector("#video-webcam");
+
+    // minta izin user
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+    // jika user memberikan izin
+    if (navigator.getUserMedia) {
+        // jalankan fungsi handleVideo, dan videoError jika izin ditolak
+        navigator.getUserMedia({ video: true }, handleVideo, videoError);
+    }
+
+    // fungsi ini akan dieksekusi jika  izin telah diberikan
+    function handleVideo(stream) {
+        video.srcObject = stream;
+    }
+
+    // fungsi ini akan dieksekusi kalau user menolak izin
+    function videoError(e) {
+        // do something
+        alert("Izinkan menggunakan webcam untuk demo!")
+    }
